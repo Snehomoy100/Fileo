0a8c2b25d46056e99eb867d979a2b5e52aef6fdb
Snehomoy Maitra <snehomoy100@gmail.com>
2022-08-12 11:26:26 +0530

    styling updated for the common loader

--- src/components/CommonLoader/CommonLoader.tsx
+++ src/components/CommonLoader/CommonLoader.tsx
@@ -2,8 +2,10 @@ import "./commonLoader.css";
 
 const CommonLoader = () => {
   return (
-    <div className="cl100Spinner">
-        <div className="cl100Circle"></div>
+    <div className="cl100Container">
+      <div className="cl100Spinner">
+          <div className="cl100Circle"></div>
+      </div>
     </div>
   );
 };
